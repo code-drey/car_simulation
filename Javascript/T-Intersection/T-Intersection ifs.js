5df3b71239b105e6ea5e8a742c55cfd7583c7899
sofiabonini <s1059520@stud.sbg.ac.at>
2020-12-08 20:33:47 +0100

    add working model

--- Javascript/T-Intersection/T-Intersection ifs.js
+++ Javascript/T-Intersection/T-Intersection ifs.js
@@ -1,44 +1,79 @@
-function priority() {
-		var carW = cars.splice(0, 1);
-		var carE = cars.splice(0, 1);
-		var carS = cars.splice(0, 1);
-		
+
+exports.setup = function () {
+	//the Input-ports for the incoming cars
+    this.input('carWEST');
+    this.input('carEAST');
+    this.input('carSOUTH');
+    //the Output-ports for the leaving cars
+    this.output('OWEST');
+    this.output('OEAST');
+    this.output('OSOUTH');
+    //the Output-ports for cars that have to wait
+    this.output('waitW');
+    this.output('waitE');
+    this.output('waitS');
+}
+
+exports.initialize = function () {
+	//creates an Array that contains the cars that stand in an Intersection
+	//following Array positions are relevant: 0 = WEST, 1 = EAST and 2 = SOUTH
+	var QueueW = [];
+	var QueueE = [];
+	var QueueS = [];
+	
+	this.addInputHandler('carWEST', function() {
+	QueueW.add(this.get('carWEST'));
+	});
+	
+	this.addInputHandler('carEAST', function() {
+	QueueE.add(this.get('carEast'));
+	});
+	
+	this.addInputHandler('carSOUTH', function() {
+	QueueS.add(this.get('carSOUTH'));
+	});
+
+	while (QueueW.length() != 0 && QueueE.length() != 0 && QueueS.length() != 0) {
 		if(carW != null) {
 			if(carW.indicator == 0) {
 				if(carS != null) {
 					if(carS.indicator == 1) {
 						//stop
+						QueueW.add(carW);
 					}else if(carS.indicator == -1) {
 						//stop
+						QueueW.add(carW);
 					}
 				}else if(carS == null) {
-					this.send('OEAST',carW);
+					this.send('OEAST',QueueW[0]);
 				}
 			}else if(carW.indicator == 1) {
 				if(carS != null) {
 					if(carS.indicator == 1) {
-						this.send('OSOUTH',carW);
+						this.send('OSOUTH',QueueW[0]);
 					}else if(carS.indicator == -1) {
-						this.send('OSOUTH',carW);
+						this.send('OSOUTH',QueueW[0]);
 					}
 				}else if(carS == null) {
-					this.send('OSouth',carW);
+					this.send('OSOUTH',QueueW[0]);
 				}
 			}
 		}
 		
 		if(carE != null) {
 			if(carE.indicator == 0) {
-				this.send('OWEST',carE);
+				this.send('OWEST',QueueE[0]);
 			}else if(carE.Indicator == -1) {
 				if(carW != null) {
 					if(carW.indicator == 0) {
 						//stop
+						QueueE.add(carE);
 					}else if(carW.indicator == 1) {
 						//stop
+						QueueE.add(carE);
 					}
 				}else if(carW == null) {
-					this.send('OWEST',carE);
+					this.send('OWEST',QueueE[0]);
 				}
 			}
 		}
@@ -49,8 +84,10 @@ function priority() {
 			}else if(carS.indicator == -1) {
 				if(carE != null) {
 					//stop
+					QueueS.add(carS);
 				}else if(carE == null) {
 					this.send('OWEST',carS);
 				}
 			}
 		}
+	}
